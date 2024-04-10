fc667ff2c0bb7e41f3ac11c778b873c4c8dd4b11
unknown <ezam0000@gmail.com>
2024-04-09 22:57:51 -0700

    updated with .exe

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const multer = require('multer');
 const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
 const fs = require('fs');
+const punycode = require('punycode/');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Configure storage for multer
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
+  destination: function (req, file, cb) {
     cb(null, 'uploads/')
   },
-  filename: function(req, file, cb) {
+  filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
   }
 });
@@ -29,6 +31,7 @@ app.post('/upload', upload.single('video'), (req, res) => {
     return res.status(400).send('No file uploaded.');
   }
 
+  // Updated logic to include 24 fps and 4 fps options
   const fpsInput = req.body.fps;
   let fps;
   switch (fpsInput) {
@@ -65,13 +68,6 @@ app.post('/upload', upload.single('video'), (req, res) => {
     .save(outputPath);
 });
 
-// Listen on port 3000 and open the browser
-app.listen(port, async () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-  try {
-    const open = (await import('open')).default;
-    open(`http://localhost:${port}`);
-  } catch (error) {
-    console.error('Failed to open browser:', error);
-  }
 });
